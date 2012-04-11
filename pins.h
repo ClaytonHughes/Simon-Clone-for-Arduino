86476ef196901a8e8e3c449b2e9881f225e57ca6
Clayton Hughes <clayton.hughes@gmail.com>
2012-04-10 17:21:09 -0700

    Cleanup - moved pin const into header.

--- pins.h
+++ pins.h
@@ -7,19 +7,19 @@ Pin definitions for Arduino hardware.
 
 // Input
 
-extern const int SNESPAD_STROBE_PIN; // orange
-extern const int SNESPAD_CLOCK_PIN; // yellow
-extern const int SNESPAD_DATA_PIN ; // red
+const int SNESPAD_STROBE_PIN = 2; // orange
+const int SNESPAD_CLOCK_PIN = 3; // yellow
+const int SNESPAD_DATA_PIN =  4; // red
 
 // Difficulty switch -- if the game starts when it's HIGH, it's hard!
-extern const int DIFF_SWITCH_PIN;
+const int DIFF_SWITCH_PIN = 13;
 
 
 // Output
-extern const int RED_LED_PIN;
-extern const int GREEN_LED_PIN;
-extern const int BLUE_LED_PIN;
-extern const int YELLOW_LED_PIN;
-extern const int SPEAKER_PIN;
+const int RED_LED_PIN = 7;
+const int GREEN_LED_PIN = 8;
+const int BLUE_LED_PIN = 9;
+const int YELLOW_LED_PIN = 10;
+const int SPEAKER_PIN = 11;
 
 #endif
